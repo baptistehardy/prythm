dd4a1e9cf6a9afb9845bb25d41b0d2fedc75a16f
Baptiste Hardy <baptiste@hardy.sh>
2024-08-21 22:44:48 +0200

    Added labels to lens tags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use exif::{In, Reader, Tag};
@@ -19,11 +20,18 @@ fn main() {
         }
     };
 
+    let map = vec![
+        (Tag::LensMake, "Maker"),
+        (Tag::LensModel, "Model"),
+        (Tag::LensSpecification, "Specs"),
+        (Tag::LensSerialNumber, "Serial Number"),
+    ];
+
     println!("Lens:\n");
 
-    for tag in [Tag::LensMake, Tag::LensModel, Tag::LensSpecification, Tag::LensSerialNumber] {
+    for (tag, label) in map {
         if let Some(field) = exif.get_field(tag, In::PRIMARY) {
-            println!("{}: {}", field.tag, field.display_value().with_unit(&exif))
+            println!("{}: {}", label, field.display_value().with_unit(&exif))
         }
     }
 }
